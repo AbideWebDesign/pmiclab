1c6d819a45a15ecfd52d7b98ec36e3ce2d894f16
Josh Armentano <josh@abidewebdesign.com>
2019-01-22 14:11:21 -0800

    Updated: Home style.

--- page-home.php
+++ page-home.php
@@ -3,12 +3,14 @@
 
 <div id="content-area">
 	<div class="container">
-		<div id="content">
-			<div class="row">
-				<div class="col-12">
-					<?php while(have_posts()): the_post(); ?>
-						<?php the_content(); ?>
-					<?php endwhile; ?>
+		<div class="container-shadow">
+			<div id="content">
+				<div class="row">
+					<div class="col-12">
+						<?php while(have_posts()): the_post(); ?>
+							<?php the_content(); ?>
+						<?php endwhile; ?>
+					</div>
 				</div>
 			</div>
 		</div>
