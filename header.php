e9a4950e3d13ef146bf4b3a23d8ff9bb4cb5b94a
Josh Armentano <josh@abidewebdesign.com>
2019-05-28 15:31:45 -0700

    Turned off bootstrap menu javascript on mobile.

--- header.php
+++ header.php
@@ -21,26 +21,33 @@
 	<?php wp_head(); ?>
 	<script type="text/javascript">
 		jQuery(function ($) {
-			// Override Bootstrap dropdown behavior
-			$('#menu-primary .dropdown > a').click(function() {
-				location.href = $(this).attr('href');
-			});
 			
-			function toggleDropdown (e) {
-				let _d = $(e.target).closest('.dropdown'),
-				 _m = $('.dropdown-menu', _d);
-				
-				setTimeout(function(){
-					let shouldOpen = e.type !== 'click' && _d.is(':hover');
-					_m.toggleClass('show', shouldOpen);
-					_d.toggleClass('show', shouldOpen);
-					$('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
-				}, e.type === 'mouseleave' ? 50 : 0);
-			}
+			<?php if ( !wp_is_mobile() ): ?>
 
-			$('body')
-				.on('mouseenter mouseleave','.dropdown',toggleDropdown)
-				.on('click', '.dropdown-menu a', toggleDropdown);
+				// Override Bootstrap dropdown behavior
+				$( '#menu-primary .dropdown > a' ).click( function() {
+					
+					location.href = $(this).attr('href');
+				
+				});
+				
+				function toggleDropdown (e) {
+					let _d = $(e.target).closest('.dropdown'),
+					 _m = $('.dropdown-menu', _d);
+					
+					setTimeout(function(){
+						let shouldOpen = e.type !== 'click' && _d.is(':hover');
+						_m.toggleClass('show', shouldOpen);
+						_d.toggleClass('show', shouldOpen);
+						$('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+					}, e.type === 'mouseleave' ? 50 : 0);
+				}
+	
+				$( 'body' )
+					.on( 'mouseenter mouseleave','.dropdown',toggleDropdown )
+					.on( 'click', '.dropdown-menu a', toggleDropdown );
+			
+			<?php endif; ?>
 			
 			// Sticky navbar
 			// =========================
