7b3e6f6ed7191f4e8f18513815aa76d5e608b6ab
Josh Armentano <josh@abidewebdesign.com>
2019-06-13 13:47:57 -0700

    Updated 404 page.

--- 404.php
+++ 404.php
@@ -10,6 +10,17 @@
 get_header();
 ?>
 
-
+<div class="py-4 bg-light">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-8 bg-white py-3">
+				<div class="text-center">
+					<h1 class="mb-2"><?php the_field('404_title', 'options'); ?></h1>
+					<div class="lead"><?php the_field('404_text', 'options'); ?></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <?php
 get_footer();
